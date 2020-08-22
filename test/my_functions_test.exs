fc0cd736608ae2c1597a6c6c11b89970745ba7c8
山根大輝 <12183597@g.matsuyama-u.ac.jp>
2020-08-22 22:36:40 +0900

    :update functions_test

--- test/my_functions_test.exs
+++ test/my_functions_test.exs
@@ -2,7 +2,23 @@ defmodule MyFunctionsTest do
   use ExUnit.Case
   doctest MyFunctions
 
-  test "greets the world" do
-    assert MyFunctions.hello() == :world
+  test "fact 5" do
+    assert MyFunctions.fact(5) == 120
+  end
+
+  test "fib 10" do
+    assert MyFunctions.fib(10) == 55
+  end
+
+  test "reverse [1, 2, 3]" do
+    assert MyFunctions.reverse([1, 2, 3]) == [3, 2, 1]
+  end
+
+  test "map([1,2,3], fn x -> x * x end)" do
+    assert MyFunctions.map([1, 2, 3], fn x -> x * x end)
+  end
+
+  test "filter([1, 2, 3], fn x -> rem(x, 2) == 0 end)" do
+    assert MyFunctions.filter([1, 2, 3], fn x -> rem(x, 2) == 0 end)
   end
 end
